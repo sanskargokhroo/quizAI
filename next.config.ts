689aaeaa848a64aa940c4c2f60e2a3e3db4c899a
sanskargokhroo1 <sanskargokhroo1@gmail.com>
2025-08-29 16:30:30 +0000

    new chang1

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,10 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-  devIndicators: {
-    allowedDevOrigins: [
-      '*.cloudworkstations.dev',
-    ],
-  },
-  serverRuntimeConfig: {
-    api: {
-      bodyParser: {
-        sizeLimit: '50mb', // Adjust this value as needed, e.g., '8mb', '16mb'
-      },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '10mb', // Increase the body size limit to 10MB
+        },
     },
-  },
 };
 
 export default nextConfig;
